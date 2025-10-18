7a170b0e5efa989dc32b0c99bd8f3d2f607157b5
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-18 14:38:50 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //print
+        System.out.print("I like pizza!\n");
+        System.out.print("It's really good.\n");
+        //println prints as a line.
+        System.out.println("Buy me pizza!");
+
+        // Type sout and then hit tab to autocomplete System.out.print 
+        System.out.println("Thank you!" );
+    }
+
+}
