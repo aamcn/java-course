81a8d536016e53e33a1b5cd6902d4f03d2a3e0d9
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-18 14:52:58 +0100

    Setup for lesson 1

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+
+    public static void main(String[] args) {
+        //print
+        System.out.print("I like pizza!\n");
+        System.out.print("It's really good.\n");
+        //println prints as a line.
+        System.out.println("Buy me pizza!");
+
+        // Type sout and then hit tab to autocomplete System.out.print 
+        System.out.println("Thank you!" );
+    }
+
+}
