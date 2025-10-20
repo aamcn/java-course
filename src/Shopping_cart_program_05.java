02f0e3bb2221cddd28e9201e939cd5693d2ce76c
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-20 13:57:46 +0100

    Lesson 5: Shopping Cart Program

---
+++ src/Shopping_cart_program_05.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Shopping_cart_program_05 {
+    public static void main(String[] args) {
+        
+    // SHOPPING CART PROGRAM WITH USER INPUT
+
+    Scanner scanner = new Scanner(System.in);
+    
+    String item;
+    double price;
+    int quantity;
+    char currency = '$';
+    double total = 0.0;
+
+    System.out.print("What item would you like to buy?");
+    item = scanner.nextLine();
+
+    System.out.print("What is the price of each");
+    price = scanner.nextDouble();  
+
+    System.out.print("How many would you like to buy?");
+    quantity = scanner.nextInt();
+
+    total = price * quantity;
+
+    System.out.println("You have purchased " + quantity + " " + item + "(s) at a price of " + currency + price + " each.");
+    System.out.println("Your total is " + currency + total + ".");
+    scanner.close();
+
+    }
+}
+ 
