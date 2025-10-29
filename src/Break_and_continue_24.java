e57ff185e4e11b2687b00ebba82bf2af55d044f5
aaronmcnulty <malcomcrabesq@outlook.com>
2025-10-29 11:16:00 +0000

    Lesson 24: Break & continue

---
+++ src/Break_and_continue_24.java
@@ -0,0 +1,20 @@
+public class Break_and_continue_24 {
+
+    public static void main(String[] args) {
+
+       // break = break out of a loop (Stop).
+       // continue = skip current iteration of a loop (Skip).
+
+        for(int i = 0; i < 10; i++){
+
+            if(i == 5){
+                continue; // Skip the number 5
+            }
+
+            System.out.print(i + "");
+
+        }
+
+    }
+    
+}
