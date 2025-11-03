f90b374aedd191fbe413f0fac21c68fdb3480f16
aaronmcnulty <malcomcrabesq@outlook.com>
2025-11-03 14:01:48 +0000

    Lesson 26: Methods

---
+++ src/Methods_26.java
@@ -0,0 +1,15 @@
+public class Methods_26 {
+    public static void main(String[] args) {
+       
+        //method = a block of code that is executed whenever it is called.
+
+        System.out.println(square(3));
+
+    }
+
+    static double square(double number) { 
+        return number * number;
+    }
+}
+
+    
